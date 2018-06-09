949b039ebb01dc57a12672fe53e3b2d218300a71
jul-ia <julie78sd@gmail.com>
2018-06-09 18:33:26 +0300

    moreProjects

---
+++ Forms/Calculator/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private const int bw = 52, bh = 31;
+
+        private const int dx = 5, dy = 5;
+
+        private Button[] btn = new Button[15];
+        char[] btnText =   {'7', '8', '9', '+',
+                            '4', '5', '6', '-',
+                            '1', '2', '3', '=',
+                                 '0', ',', 'c'};
+        
+        int[] btnTag ={ 7, 8, 9, -3,
+                        4, 5, 6, -4,
+                        1, 2, 3, -2,
+                        0, -1, -5 };
+
+        private double ac = 0;
+        private int op = 0;
+        private Boolean fd = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ClientSize = new Size(4 * bw + 5 * dx, 5 * bh + 7 * dy);
+            label1.SetBounds(dx, dy, 4 * bw + 3 * dx, bh);
+            label1.Text = "0";
+            label1.AutoSize = false;
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            int k = 0, x, y;
+            y = label1.Bottom + dy;
+
+            for (int i = 0; i < 4; i++)
+            {
+                x = dx;
+                for (int j = 0; j < 4; j++)
+                {
+                    if (!(i == 3 && j == 0))
+                    {
+                        btn[k] = new Button();
+                        btn[k].SetBounds(x, y, bw, bh);
+                        btn[k].Tag = btnTag[k];
+                        btn[k].Text = btnText[k].ToString();
+
+                        this.btn[k].Click += new System.EventHandler(this.Button_Click);
+
+                        if (btnTag[k] < 0)
+                        {
+                            btn[k].BackColor = SystemColors.ControlLight;
+                        }
+                        this.Controls.Add(this.btn[k]);
+                        x += bw + dx;
+                        k++;
+                    }
+                    else
+                    {
+                        btn[k] = new Button();
+                        btn[k].SetBounds(x, y, bw * 2 + dx, bh);
+                        btn[k].Tag = btnTag[k];
+                        btn[k].Text = btnText[k].ToString();
+
+                        this.btn[k].Click += new System.EventHandler(this.Button_Click);
+                        this.Controls.Add(this.btn[k]);
+                        x += 2 * bw + 2 * dx;
+                        k++;
+                        j++;
+                    }
+                }
+                y += bh + dy;
+            }
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (Convert.ToInt32(btn.Tag) > 0)
+            {
+                if (fd)
+                {
+                    label1.Text = btn.Text;
+                    fd = false;
+                }
+                else
+                {
+                    label1.Text += btn.Text;
+                }
+                return;
+            }
+            if (Convert.ToInt32(btn.Tag) == 0)
+            {
+                if (fd)
+                    label1.Text = btn.Text;
+                if (label1.Text != "0")
+                    label1.Text += btn.Text;
+                return;
+            }
+            if (Convert.ToInt32(btn.Tag) == -1)
+            {
+                if (fd)
+                {
+                    label1.Text = "0,";
+                    fd = false;
+                }
+                else
+                {
+                    if (label1.Text.IndexOf(",") == -1)
+                        label1.Text += btn.Text;
+                }
+                return;
+            }
+            if (Convert.ToInt32(btn.Tag) == -5)
+            {
+                ac = 0;
+                op = 0;
+                label1.Text = "0";
+                fd = true;
+                return;
+            }
+            if (Convert.ToInt32(btn.Tag) < -1)
+            {
+                double n;
+                n = Convert.ToDouble(label1.Text);
+                if (ac != 0)
+                {
+                    switch (op)
+                    {
+                        case -3: ac += n; break;
+                        case -4: ac -= n; break;
+                        case -2: ac = n; break;
+                    }
+                    label1.Text = ac.ToString("N");
+                }
+                else
+                {
+                    ac = n;
+                }
+                op = Convert.ToInt32(btn.Tag);
+                fd = true;
+            }
+        }
+    }
+}
---
+++ Forms/ViewImages/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ViewImages
+{
+    public partial class Form1 : Form
+    {
+        int pbw, pbh, pbX, pbY; // розмір і положення Picture Box
+
+
+        string aPath;
+
+        
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+
+            pbh = pictureBox1.Height;
+            pbw = pictureBox1.Width;
+            pbX = pictureBox1.Location.X;
+            pbY = pictureBox1.Location.Y;
+
+            listBox1.Sorted = true;
+
+            DirectoryInfo di = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+            aPath = di.FullName;
+            label1.Text = aPath;
+            FillListBox(aPath);
+        }
+        
+        private Boolean FillListBox(string aPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(aPath);
+            FileInfo[] fi = di.GetFiles("*.png");
+            listBox1.Items.Clear();
+            foreach(FileInfo fc in fi)
+            {
+                listBox1.Items.Add(fc.Name);
+            }
+
+            fi = di.GetFiles("*.jpg");
+            foreach (FileInfo fc in fi)
+            {
+                listBox1.Items.Add(fc.Name);
+            }
+
+            label1.Text = aPath;
+            if (fi.Length == 0) return false;
+            else
+            {
+                listBox1.SelectedIndex = 0;
+                return true;
+            }
+        }
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double mh, mw;
+
+            pictureBox1.Visible = false;
+            pictureBox1.Left = pbX;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Image = new Bitmap(aPath + "\\" + listBox1.SelectedItem.ToString());
+
+            if((pictureBox1.Image.Width > pbw) || (pictureBox1.Image.Height > pbh))
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                mh = (double)pbh / (double)pictureBox1.Image.Height;
+                mw = (double)pbw / (double)pictureBox1.Image.Width;
+                if(mh < mw)
+                {
+                    pictureBox1.Width = Convert.ToInt16(pictureBox1.Image.Width * mh);
+                    pictureBox1.Height = pbh;
+                }
+                else
+                {
+                    pictureBox1.Width = pbw;
+                    pictureBox1.Height = Convert.ToInt16(pictureBox1.Image.Height * mw);
+                }
+            }
+            pictureBox1.Left = pbX + (pbw - pictureBox1.Width) / 2;
+            pictureBox1.Top = pbY + (pbh - pictureBox1.Height) / 2;
+            pictureBox1.Visible = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.Description = "Выберите папку, \nв какой находятся иллюстрации";
+            fb.ShowNewFolderButton = false;
+            if(fb.ShowDialog() == DialogResult.OK)
+            {
+                aPath = fb.SelectedPath;
+                label1.Text = aPath;
+
+                if (!FillListBox(fb.SelectedPath))
+                    pictureBox1.Image = null;
+            } 
+        }
+    }
+}
